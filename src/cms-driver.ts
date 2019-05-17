23e4fb7336e4a5f1a4a3224a350878d98adffd00
KoeMai <github@mairbaeurl.de>
2019-05-18 01:55:22 +0200

    Replaced pickGenerator by variable for generator
    The cms client normally started a pick call in the UI.
    This modification moves the questions infront of the start.
    This removes the required callback from cms client.

--- src/cms-driver.ts
+++ src/cms-driver.ts
@@ -255,7 +255,7 @@ export class CMakeServerClientDriver extends CMakeDriver {
       onProgress: async prog => {
         this._progressEmitter.fire(prog);
       },
-      pickGenerator: () => this.getBestGenerator(),
+      generator: await this.getBestGenerator(),
     });
   }
 
